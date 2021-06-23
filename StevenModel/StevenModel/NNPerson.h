9fed810051fb23ebeef970ce9bb0420ee11a3a45
lifangjian <417962507@qq.com>
2021-06-23 13:43:10 +0800

    肉肉肉

--- StevenModel/StevenModel/NNPerson.h
+++ StevenModel/StevenModel/NNPerson.h
@@ -2,8 +2,8 @@
 //  NNPerson.h
 //  NNRuntimeTest
 //
-//  Created by liupengkun on 2018/6/21.
-//  Copyright © 2018年 以梦为马. All rights reserved.
+//  Created by 李方建 on 2018/8/1.
+//  Copyright © 2018年 李方建. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
